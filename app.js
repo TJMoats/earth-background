f302873d6cf3ac029b1fa881bea37dd2c7944fec
TJMoats <james.moats@vonage.com>
2016-06-10 10:46:39 -0700

    formatting

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
-var CronJob = require('cron').CronJob;
-var moment = require('moment');
-var fs = require('fs-extra');
-var request = require('request');
-var Canvas = require('canvas');
-var Image = Canvas.Image;
+var CronJob   = require('cron').CronJob;
+var moment    = require('moment');
+var fs        = require('fs-extra');
+var request   = require('request');
+var Canvas    = require('canvas');
+var Image     = Canvas.Image;
 var wallpaper = require('wallpaper');
-var async = require("async");
+var async     = require("async");
 
-var zone_offset = 0;
-var width = 550;
-var level = "4d"; //Level can be 4d, 8d, 16d, 20d
-var numblocks = 4; //this apparently corresponds directly with the level, keep this exactly the same as level without the 'd'
-var canvas_size = width * numblocks;
+var width         = 550;
+var level         = "4d"; //Level can be 4d, 8d, 16d, 20d
+var numblocks     = 4; //this apparently corresponds directly with the level, keep this exactly the same as level without the 'd'
+var canvas_size   = width * numblocks;
 var local_storage = __dirname + '/images/';
-var out_file = 'latest.png';
 
-function init(cb)
-{
+var desktop_width = 1920;
+var desktop_height = 1080;
+
+function init(cb) {
 	fs.ensureDir(local_storage, function (err) {
-		if (err){
+		if (err) {
 			cb(err);
 		} else {
 			cb(null);
@@ -26,27 +26,26 @@ function init(cb)
 	})
 }
 
-function getImageBlocks(cb)
-{
+function getImageBlocks(cb) {
 	console.log('Downloading image blocks.');
-	var hours = moment().format('HH');
+	var hours   = moment().format('HH');
 	var minutes = moment().format('mm');
-	minutes = (parseInt(minutes) - (parseInt(minutes) % 10)).toString();
+	minutes     = (parseInt(minutes) - (parseInt(minutes) % 10)).toString();
 	var seconds = '00';
-	
-	var time = hours + minutes + seconds;
-	var year = moment().format('YYYY');
-	var month = moment().format('MM');
-	var day = moment().format('DD');
 
-	var url = "http://himawari8-dl.nict.go.jp/himawari8/img/D531106/" + level + "/" + width + "/" + year + "/" + month + "/" + day + "/" + time;
+	var time  = hours + minutes + seconds;
+	var year  = moment().format('YYYY');
+	var month = moment().format('MM');
+	var day   = moment().format('DD');
+
+	var url     = "http://himawari8-dl.nict.go.jp/himawari8/img/D531106/" + level + "/" + width + "/" + year + "/" + month + "/" + day + "/" + time;
 	var counter = 1;
-	for(var y=0;y<numblocks;y++){
-		for(var x=0;x<numblocks;x++){
+	for (var y = 0; y < numblocks; y++) {
+		for (var x = 0; x < numblocks; x++) {
 			var file_name = "_" + x + "_" + y + ".png";
-			var this_url = url + "_" + x + "_" + y + ".png";
-			download(this_url, local_storage + 'block' + file_name, function(){
-				if ((counter++) >= numblocks*numblocks){
+			var this_url  = url + "_" + x + "_" + y + ".png";
+			download(this_url, local_storage + 'block' + file_name, function () {
+				if ((counter++) >= numblocks * numblocks) {
 					cb(null);
 				}
 			});
@@ -54,86 +53,116 @@ function getImageBlocks(cb)
 	}
 }
 
-function stitchImageBlocks(cb)
-{
+function stitchImageBlocks(cb) {
 	console.log('Stitching image blocks.');
 
-	var canvas = new Canvas(canvas_size, canvas_size),
-	    ctx = canvas.getContext('2d');
+	var max_size = desktop_width < desktop_height ? desktop_width : desktop_height;
+	var default_size = width * numblocks;
+	var scale = max_size / default_size;
+
+	var out_file = 'latest_' + moment().format('YYYYMMDDHHmm') + '.png';
+	var canvas   = new Canvas(desktop_width, desktop_height),
+	    ctx      = canvas.getContext('2d');
 
 	var counter = 1;
-	for(var y=0;y<numblocks;y++){
-		for(var x=0;x<numblocks;x++){
+	for (var y = 0; y < numblocks; y++) {
+		for (var x = 0; x < numblocks; x++) {
 			var file_name = "_" + x + "_" + y + ".png";
 
 			//check to make sure the images exist
-			if (!fs.existsSync(local_storage + 'block' + file_name, fs.F_OK)){
+			if (!fs.existsSync(local_storage + 'block' + file_name, fs.F_OK)) {
 				console.err('File missing. Aborting.');
 				return false;
 			}
 
-			var img = new Image;
-			img.onload = function(){
-				ctx.drawImage(img, x*width, y*width, img.width, img.height);
-				if ((counter++) >= numblocks*numblocks){
-					canvas.toBuffer(function(err, buf){
-						if (err)
+			var img    = new Image;
+			img.onload = function () {
+				ctx.drawImage(img, x * width, y * width, img.width, img.height);
+				if ((counter++) >= numblocks * numblocks) {
+					canvas.toBuffer(function (err, buf) {
+						if (err) {
 							cb(err);
-						else
+						} else {
 							console.log('Writing file ' + local_storage + out_file);
-							fs.writeFile(local_storage + out_file, buf, function(err){
-								if (err) {
-									cb(err);
-								} else {
-									cb(null);
-								}
-							});
+						}
+						fs.writeFile(local_storage + out_file, buf, function (err) {
+							if (err) {
+								cb(err);
+							} else {
+								cb(null, out_file);
+							}
+						});
 					});
 				}
 			};
-			img.src = local_storage + 'block' + file_name;
+			img.src    = local_storage + 'block' + file_name;
 		}
 	}
 }
 
-function setBackground(cb)
-{
+function setBackground(file_name, cb) {
 	console.log('Setting background.');
-	wallpaper.set(local_storage + out_file).then(function(){
+	wallpaper.set(local_storage + file_name).then(function () {
 		cb(null);
 	});
 }
 
-function download(uri, filename, callback){
-  request.head(uri, function(err, res, body){
-    request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
-  });
+function cleanUp(cb){
+	fs.emptyDir(local_storage, function (err) {
+		if (err) {
+			cb(err);
+		} else {
+			cb(null);
+		}
+	});
 }
 
-function run(cb){
+function download(uri, filename, callback) {
+	request.head(uri, function (err, res, body) {
+		request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
+	});
+}
+
+function run(cb) {
 	console.log('Running task.');
-	async.series([
-		getImageBlocks,
-		stitchImageBlocks,
-		setBackground,
-	    function(){
-		    cb(null);
-	    }
-	]);
+	cleanUp(function(err){
+		if (err){
+			throw err;
+		}
+		getImageBlocks(function (err) {
+			if (err) {
+				throw err;
+			} else {
+				stitchImageBlocks(function (err, filename) {
+					console.log(filename);
+					if (err) {
+						throw err;
+					} else {
+						setBackground(filename, function (err) {
+							if (err) {
+								throw err;
+							} else {
+								cb(null)
+							}
+						})
+					}
+				})
+			}
+		});
+	});
 }
 
-run(function(){
-	console.log('done');
-});
-
-init(function(err){
-	if(err){
+init(function (err) {
+	if (err) {
 		throw err;
 	}
 
-	var job = new CronJob('*/5 * * * *', run, null,
-		true, // Start the job right now
-		'America/Phoenix' //Time zone of this job.
-	);
+	run(function (err) {
+		if (err){
+			throw err;
+		} else {
+			console.log('Done.');
+		}
+	});
 });
 
